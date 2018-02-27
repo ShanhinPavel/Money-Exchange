a1e47fdc60e986f4526834afaf4eb34803133739
ShanhinPavel <shanMail@tut.by>
2018-02-27 18:59:02 +0300

    Fifth task has solved

--- src/index.js
+++ src/index.js
@@ -2,4 +2,33 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+   let exchangedMoney = {};
+   let objCoins = {
+     "H":50,
+     "Q":25,
+     "D":10,
+     "N":5,
+     "P":1
+   };
+   let newCurrency = currency;
+   
+   if (currency >= 10000) {
+    exchangedMoney.error = "You are rich, my friend! We don't have so much coins for exchange";
+   } else {
+       if (currency > 0) {
+
+   for (key in objCoins) {
+     if (newCurrency >= objCoins[key]) {
+       exchangedMoney[key] = Math.floor(newCurrency / objCoins[key]);
+       newCurrency = newCurrency % objCoins[key];
+     }
+     if (newCurrency === 0) {
+       break;
+     } else {
+       continue;
+     }
+   }
+}
+   }
+  return exchangedMoney;
 }
